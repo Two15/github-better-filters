5f57d9e8d399ec11f5c8f4223d3b628fd819ffd7
Xavier Cambar <xcambar@gmail.com>
2015-07-10 11:38:42 +0200

    Shows relevant links for Chrome and Firefox

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+"use strict";
 
+var ua = detect.parse(navigator.userAgent);
+
+function $find(selector) {
+  return Array.prototype.slice.call(document.querySelectorAll(selector));
+}
+
+function hide ($e) {
+  $e.className = 'hidden';
+}
+
+var lowerCasedName = ua.browser.family.toLowerCase();
+if (lowerCasedName.match("firefox")) {
+  $find('.chrome').forEach(hide);
+} else if (lowerCasedName.match("chrome")) {
+  $find('.firefox').forEach(hide);
+} else {
+  $find('.alternative').forEach(hide);
+}
